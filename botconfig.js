3be225a895219013165ae1d054c22690b6cdfd05
Dmitriy <109019828+alkyqekasy@users.noreply.github.com>
2023-06-22 21:13:25 +0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", // Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
+  Token: process.env.Token || "MTEyMTQzMjExNDIwMjY4MTM1NA.Gs5XeH.DcmyqAYQwx2p7RVF6sObX_GidazQRl2KhaJ5pU", // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", // Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
